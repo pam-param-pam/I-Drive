cacb567cd9ad769312c837adce780f2905945a68
zbyszekm <zbigniew.malachowski@gmail.com>
2025-03-02 20:23:37 +0100

    initial version2

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 curl https://raw.githubusercontent.com/pam-param-pam/I-Drive/master/docker-compose.yml > docker-compose.yml
-curl https://raw.githubusercontent.com/pam-param-pam/I-Drive/master/.env 
-docker compose up
-docker exec -it idrive-backend bash
-ls
-exit
-          
+curl https://raw.githubusercontent.com/pam-param-pam/I-Drive/master/nginx.conf > nginx.conf
+docker compose up & 
+
+
+while [[ $(docker inspect -f '{{.State.Status}}' idrive-backend) != 'running' ]]
+do
+   echo "waiting for idrive-backend to start"
+   sleep 1s
+done
+
+docker exec idrive-backend echo "----------------I have been in the container -------------"
+echo " ----------------------------------------- sleeping --------------------------------"
+sleep 80s
+echo "----------------------------------------STOPPING --------------------------------"
+docker compose stop
