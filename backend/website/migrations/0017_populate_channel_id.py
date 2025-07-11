4c4241e9b0f401da170bbb8079f34c4a908ab63e
Pamparampam <you@example.com>
2025-07-11 03:42:40 +0200

    bug fixes

--- backend/website/migrations/0017_populate_channel_id.py
+++ backend/website/migrations/0017_populate_channel_id.py
@@ -16,11 +16,14 @@ def populate_channel_ids(apps, schema_editor):
     model_names = ['Fragment', 'Thumbnail', 'Preview', 'Moment', 'Subtitle']
     for model_name in model_names:
         Model = apps.get_model('website', model_name)
+        webhook_channel_id = None
         for obj in Model.objects.select_related('content_type').all():
             ct = obj.content_type
             if ct.model == 'bot':
-                channel_id = input("Please type channel_id")
+                if not webhook_channel_id:
+                    webhook_channel_id = input("Please type channel_id: ")
 
+                channel_id = webhook_channel_id
             elif ct.model == 'webhook':
                 author = Webhook.objects.get(discord_id=obj.object_id)
                 channel_id = author.channel.id
